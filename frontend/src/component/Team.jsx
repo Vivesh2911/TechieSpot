58f0cbab38e4feac070f5c526ca187c6a1a2e52a
Vivesh2911 <vivesh2911@gmail.com>
2024-02-29 14:18:21 +0530

    techmemes

--- frontend/src/component/Team.jsx
+++ frontend/src/component/Team.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 import styles from "./Team.module.css";
 export default function Team() {
   return (
-    <div className={styles.team}>
-        Team
+    // <center>
+    <div classNameName={styles.team}>
+        <div className={styles.card} >
+  <img src="https://images.pexels.com/photos/1559486/pexels-photo-1559486.jpeg?auto=compress&cs=tinysrgb&w=800" className={styles.top} alt="..."/>
+  <div className={styles.body}>
+    <h5 className="card-title">Developer - 1</h5>
+    <p className="card-text">Hello , i am developer who develop this area . This is my area </p>
+    <a href="#" className="btn btn-primary">About me </a>
+  </div>
+</div>
     </div>
+   
   )
 }
--- frontend/src/memes/Meme.jsx
+++ frontend/src/memes/Meme.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import NavBar from '../component/NavBar'
-
+import List from '../component/List'
+import Memepost from '../component/Memepost'
 export default function Meme() {
   return (
     <>
       <NavBar/>
+    <List/>
+      <Memepost/>
     </>
   )
 }
